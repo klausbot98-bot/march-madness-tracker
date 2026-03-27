fa1a3f1c046704104838219ff20338bb0ea52f22
Kevin Vertkas <vertkaskevin@gmail.com>
2026-03-27 15:18:40 -0400

    Sync tracker state through repo data.json

--- api/data.js
+++ api/data.js
@@ -1,11 +1,11 @@
-import { readData } from "../lib/data-store.js";
-import { sendJson, sendMethodNotAllowed } from "../lib/api-response.js";
+import { readData, writeData } from "../lib/data-store.js";
+import { readBody, sendJson, sendMethodNotAllowed } from "../lib/api-response.js";
 
 export default async function handler(req, res) {
-  if (req.method !== "GET") return sendMethodNotAllowed(req, res, ["GET"]);
+  if (!["GET", "POST"].includes(req.method)) return sendMethodNotAllowed(req, res, ["GET", "POST"]);
 
   try {
-    const data = await readData();
+    const data = req.method === "GET" ? await readData() : await writeData(readBody(req));
     sendJson(res, 200, data);
   } catch (error) {
     sendJson(res, 500, { error: error.message });
